b12a71c0d1c16715b90b56d9ec9d64979867b4de
neoinubuntu <neolinjian@gmail.com>
2017-11-16 21:38:00 +0800

    20171116-1

---
+++ rwemf/tex/clear.bat
@@ -0,0 +1,6 @@
+del *.aux
+del *.fdb_latexmk
+del *.fls
+del *.log
+del *.pdf
+del *.synctex.gz
---
+++ rwemf/tex/clear.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+rm -rf *.aux
+rm -rf *.fdb_latexmk
+rm -rf *.fls
+rm -rf *.log
+rm -rf *.pdf
+rm -rf *.synctex.gz
+
